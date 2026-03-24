3432a6d4ff9049273f00c65192bf84ddbc41c30a
dCben335 <benoit.cabocel.335@gmail.com>
2026-03-24 21:04:09 +0100

    UPDATE typing

--- src/components/customs/I18nLink.tsx
+++ src/components/customs/I18nLink.tsx
@@ -2,10 +2,8 @@ import type { ReactNode, ComponentProps } from 'react';
 import { Link } from '@/i18n/navigation';
 import { getPath, type RouteKey, type Locale } from '@/i18n/routing';
 
-type LinkProps = ComponentProps<typeof Link>;
 
-
-type I18nLinkProps = Omit<LinkProps, 'href'> & {
+export type I18nLinkProps = ComponentProps<typeof Link> & {
 	href: RouteKey;
 	locale?: Locale;
 	children: Readonly<ReactNode>;
@@ -18,8 +16,10 @@ export const I18nLink = ({
 	children,
 	...rest
 }: I18nLinkProps) => {
+	const hrefPath = getPath(href);
+
 	return (
-		<Link href={getPath(href) as LinkProps['href']} locale={locale} {...rest}>
+		<Link href={hrefPath} locale={locale} {...rest}>
 			{children}
 		</Link>
 	);

d680757b6a78ff47aa71b246f0acc8014facac51
Benoit CABOCEL <bcabocel@student.42lyon.fr>
2026-04-03 18:23:52 +0200

    CREATE Main Layout

--- src/app/[locale]/(client)/posts/page.tsx
+++ src/app/[locale]/(client)/posts/page.tsx
@@ -5,18 +5,20 @@ const Page = () => {
 		<div className='container mx-auto px-4 py-8'>
 			<h1 className='text-3xl font-bold mb-4'>Posts</h1>
 			<p className='text-foreground'>This is the posts page.</p>
-			{Array.from({ length: 5 }, (_, i) => (
-				<I18nLink
-					key={`i18n-${i}`}
-					href='postId'
-					params={{
-						id: (i + 1).toString(),
-					}}
-					className='p-4 border rounded-md mb-4'>
-					<h2 className='text-xl font-semibold mb-2'>Post {i + 1}</h2>
-					<p>This is the content of post {i + 1}.</p>
-				</I18nLink>
-			))}
+			<div className='space-y-4'>
+				{Array.from({ length: 5 }, (_, i) => (
+					<div key={`i18n-${i}`} className='p-4 border rounded-md'>
+						<I18nLink
+							href='postId'
+							params={{
+								id: (i + 1).toString(),
+							}}>
+							<h2 className='text-xl font-semibold mb-2'>Post {i + 1}</h2>
+							<p>This is the content of post {i + 1}.</p>
+						</I18nLink>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
--- src/components/customs/Nav.tsx
+++ src/components/customs/Nav.tsx
@@ -1,6 +1,8 @@
-import { Link } from '@/i18n/navigation';
-import { getRoute, RouteKey } from '@/i18n/routing';
+import I18nLink from './I18nLink';
+import { RouteKey } from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
+import { Button } from '../ui/button';
+import { cn } from '@/libs/utils';
 
 interface NavLink {
 	key: RouteKey;
@@ -18,17 +20,47 @@ const navLinks: NavLink[] = [
 
 interface NavProps extends React.HTMLAttributes<HTMLElement> {
 	className?: string;
+	align?: 'left' | 'center' | 'right';
+	linkWidth?: 'fit' | 'full';
 }
 
-const Nav = ({ ...props }: NavProps) => {
+type Align = NonNullable<NavProps['align']>;
+type LinkWidth = NonNullable<NavProps['linkWidth']>;
+
+type AlignClass = Record<Align, string>;
+type LinkWidthClass = Record<LinkWidth, string>;
+
+const alignX = {
+	left: 'justify-start',
+	center: 'justify-center',
+	right: 'justify-end',
+} satisfies AlignClass;
+
+const alignY = {
+	left: 'items-start',
+	center: 'items-center',
+	right: 'items-end',
+} satisfies AlignClass;
+
+const linkWidthClass = {
+	fit: 'w-auto',
+	full: 'w-full',
+} satisfies LinkWidthClass;
+
+const Nav = ({ className, align = 'left', linkWidth = 'full', ...props }: NavProps) => {
 	const t = useTranslations('nav');
 
 	return (
-		<nav {...props}>
-			<ul>
+		<nav className={cn(className)} {...props}>
+			<ul className={`flex space-y-4 flex-col ${alignY[align]}`}>
 				{navLinks.map((link) => (
-					<li key={link.key}>
-						<Link href={getRoute(link.key)}>{t(`${link.labelKey}`)}</Link>
+					<li key={link.key} className={`${linkWidthClass[linkWidth]}`}>
+						<Button
+							variant='ghost'
+							className={`w-full ${alignX[align]}`}
+							asChild>
+							<I18nLink href={link.key}>{t(link.labelKey)}</I18nLink>
+						</Button>
 					</li>
 				))}
 			</ul>

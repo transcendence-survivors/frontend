f001c7a4bc8666c0e4ef1c6c0cafcef15531a989
dCben335 <benoit.cabocel.335@gmail.com>
2026-06-10 19:44:42 +0200

    ADD Signup Page title and already have account placeholders (need uselocale)

--- src/app/[locale]/(auth)/register/page.tsx
+++ src/app/[locale]/(auth)/register/page.tsx
@@ -1,11 +1,22 @@
+import { Button } from '@/components/ui/button';
 import SignUpForm from '@/features/auth/components/SignUpForm';
+import { I18nLink } from '@/modules/i18n/components/I18nLink';
 
 export default function LoginPage() {
 	return (
-		<main className='flex min-h-screen items-center justify-center'>
-			<div className='w-lg'>
+		<main className='flex min-h-[85vh] items-center justify-center'>
+			<section className='max-w-lg p-4 w-full'>
+				<div className='space-y-1'>
+					<h1>Register</h1>
+					<p className='text-sm text-muted-foreground '>
+						<span>Already have an account?&nbsp;</span>
+						<Button variant='link' asChild className='p-0 select-auto h-auto'>
+							<I18nLink href='login'>Sign in</I18nLink>
+						</Button>
+					</p>
+				</div>
 				<SignUpForm />
-			</div>
+			</section>
 		</main>
 	);
 }

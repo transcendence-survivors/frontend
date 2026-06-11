a3adf952bafd9475c2367c720d1fb38a851fc53a
Benoit CABOCEL <bcabocel@student.42lyon.fr>
2026-06-11 14:03:46 +0200

    ADJUST login and register pages

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -5,21 +5,19 @@ import { getTranslations } from 'next-intl/server';
 
 export default async function LoginPage() {
 	const t = await getTranslations('auth.signin');
+
 	return (
-		<main className='flex flex-col min-h-[90vh] items-center justify-center '>
+		<main className='flex flex-col min-h-[85vh] items-center justify-center '>
 			<section className='w-full max-w-lg px-4 space-y-4'>
-				<div className='mt-2'>
+				<div>
 					<h1>{t('title')}</h1>
-					<p className='text-sm text-muted-foreground'>
-						<span>{t('account_exists')}&nbsp;</span>
-						<Button
-							variant='link'
-							size='sm'
-							className='select-auto p-0 h-auto'
-							asChild>
-							<I18nLink href='register'>{t('sign_up')}</I18nLink>
-						</Button>
-					</p>
+					<Button
+						variant='link'
+						size='sm'
+						className='select-auto p-0 h-auto text-muted-foreground'
+						asChild>
+						<I18nLink href='register'>{t('no_account')}</I18nLink>
+					</Button>
 				</div>
 				<SignInForm />
 			</section>
--- src/app/[locale]/(auth)/register/page.tsx
+++ src/app/[locale]/(auth)/register/page.tsx
@@ -1,19 +1,22 @@
 import { Button } from '@/components/ui/button';
 import SignUpForm from '@/features/auth/components/SignUpForm';
 import { I18nLink } from '@/modules/i18n/components/I18nLink';
+import { getTranslations } from 'next-intl/server';
 
-export default function LoginPage() {
+export default async function LoginPage() {
+	const t = await getTranslations('auth.signup');
+
 	return (
-		<main className='flex min-h-[85vh] items-center justify-center'>
-			<section className='max-w-lg p-4 w-full'>
-				<div className='space-y-1'>
-					<h1>Register</h1>
-					<p className='text-sm text-muted-foreground '>
-						<span>Already have an account?&nbsp;</span>
-						<Button variant='link' asChild className='p-0 select-auto h-auto'>
-							<I18nLink href='login'>Sign in</I18nLink>
-						</Button>
-					</p>
+		<main className='flex flex-col min-h-[85vh] items-center justify-center'>
+			<section className='w-full max-w-lg px-4 space-y-4'>
+				<div>
+					<h1>{t('title')}</h1>
+					<Button
+						variant='link'
+						asChild
+						className='p-0 select-auto h-auto text-sm text-muted-foreground'>
+						<I18nLink href='login'>{t('already_account')}</I18nLink>
+					</Button>
 				</div>
 				<SignUpForm />
 			</section>

a38728b9408dcf554b847be3b9f837fbe4a4efc6
Noa Fanizzi <noafanizzistdy@gmail.com>
2026-07-04 13:00:53 +0200

    REWORK Important refacto for the structure of the posts.tsx

--- src/features/posts/components/posts.tsx
+++ src/features/posts/components/posts.tsx
@@ -4,6 +4,7 @@ import { useInView } from 'react-intersection-observer';
 import { usePosts } from '../hook/usePosts';
 import { useEffect } from 'react';
 import { Spinner } from '@/components/ui/spinner';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 
 export default function Posts() {
 	const { ref, inView } = useInView({
@@ -26,32 +27,41 @@ export default function Posts() {
 
 	return (
 		<>
-			<ul className='container mx-auto px-4 py-8'>
+			<ul className='max-w-xl mx-auto px-4 py-8 list-none'>
 				{posts.map((p) => (
-					<li key={p.id} className='p-4 border rounded-md'>
-						<div className='font-semibold'>
-							{' '}
-							{p.author?.displayName ?? p.author.id}
-						</div>
-						<div className='text-foreground'>{p.content}</div>
-						<div className='text-foreground'>
+					<li
+						key={p.id}
+						className='flex gap-3 p-4 border-b hover:bg-muted/50 transition-colors cursor-pointer'>
+						<Avatar>
+							<AvatarImage src={p.author.avatarUrl} />
+							<AvatarFallback>
+								{p.author.displayName.charAt(0)}
+							</AvatarFallback>
+						</Avatar>
+						<div>
+							<div className='flex gap-2'>
+								<span className='font-semibold'>
+									{' '}
+									{p.author?.displayName ?? p.author.id}
+								</span>
+								<span className='text-sm text-muted-foreground'>
+									{new Date(p.createdAt).toLocaleString('fr-FR', {
+										day: '2-digit',
+										month: '2-digit',
+										year: 'numeric',
+										hour: '2-digit',
+										minute: '2-digit',
+									})}
+								</span>
+							</div>
+							<p>{p.content}</p>
 							{p.imageUrl && (
 								<img
 									src={p.imageUrl}
 									alt=''
-									className='mt-2 w-full max-h-96 object-cover rounded-md'
+									className='mt-2 w-full max-h-96 object-cover rounded-2xl border border-border'
 								/>
 							)}
-						</div>
-						<div className='text-foreground'>
-							<br />
-							{new Date(p.createdAt).toLocaleString('fr-FR', {
-								day: '2-digit',
-								month: '2-digit',
-								year: 'numeric',
-								hour: '2-digit',
-								minute: '2-digit',
-							})}
 						</div>
 					</li>
 				))}
@@ -64,3 +74,77 @@ export default function Posts() {
 		</>
 	);
 }
+
+// 'use client';
+
+// import { useInView } from 'react-intersection-observer';
+// import { usePosts } from '../hook/usePosts';
+// import { useEffect } from 'react';
+// import { Spinner } from '@/components/ui/spinner';
+// import { Avatar } from 'radix-ui';
+// import { AvatarImage } from '@/components/ui/avatar';
+
+// export default function Posts() {
+// 	const { ref, inView } = useInView({
+// 		rootMargin: '0px 0px 100px 0px',
+// 	});
+// 	const { data, isLoading, isError, hasNextPage, fetchNextPage, isFetchingNextPage } =
+// 		usePosts();
+// 	useEffect(() => {
+// 		if (!hasNextPage) return;
+// 		if (!inView) return;
+// 		if (isFetchingNextPage) return;
+
+// 		fetchNextPage();
+// 	}, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+
+// 	if (isLoading) return 'Loading';
+// 	if (isError) return 'Error loading posts';
+// 	if (!data || data.pages.length === 0) return 'Pas de data';
+// 	const posts = data.pages.flatMap((page) => page.data.data);
+
+// 	return (
+// 		<>
+// 			<ul className='container mx-auto px-4 py-8'>
+// 				{posts.map((p) => (
+// 					<li key={p.id} className='flex gap-3 p-4 border-b'>
+// 						<div className='font-semibold'>
+// 							{' '}
+// 							<Avatar>
+// 								{p.author.avatarUrl && (
+// 									<AvatarImage src={p.author.avatarUrl} />
+// 								)}
+// 							</Avatar>
+// 							{p.author?.displayName ?? p.author.id}
+// 						</div>
+// 						<div className='text-foreground'>{p.content}</div>
+// 						<div className='text-foreground'>
+// 							{p.imageUrl && (
+// 								<img
+// 									src={p.imageUrl}
+// 									alt=''
+// 									className='mt-2 w-full max-h-96 object-cover rounded-md'
+// 								/>
+// 							)}
+// 						</div>
+// 						<div className='text-foreground'>
+// 							<br />
+// 							{new Date(p.createdAt).toLocaleString('fr-FR', {
+// 								day: '2-digit',
+// 								month: '2-digit',
+// 								year: 'numeric',
+// 								hour: '2-digit',
+// 								minute: '2-digit',
+// 							})}
+// 						</div>
+// 					</li>
+// 				))}
+// 			</ul>
+// 			{hasNextPage && (
+// 				<div ref={ref} className='flex justify-center py-4'>
+// 					{isFetchingNextPage && <Spinner className='size-6' />}
+// 				</div>
+// 			)}
+// 		</>
+// 	);
+// }

e786043621398760a017f1d379d488779610b81f
Benoit CABOCEL <bcabocel@student.42lyon.fr>
2026-07-25 18:46:04 +0200

    FIX game page css to take all the page

--- src/app/[locale]/game/page.tsx
+++ src/app/[locale]/game/page.tsx
@@ -2,7 +2,7 @@ import { GameRoot } from '@/features/game/components/GameRoot';
 
 export default function Page() {
 	return (
-		<div className='w-full h-full'>
+		<div className='w-screen h-screen'>
 			<GameRoot />
 		</div>
 	);
